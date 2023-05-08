79e9904f5efd68a1f44be97ac58982fe7c792a5b
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-05-08 09:21:01 +0630

    Add title and descriptions

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
-const Page = () => {
+const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        <p className="desc text-center">
+          Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
+
+        {/* Feed */}
+      </h1>
+    </section>
   )
 }
 
-export default Page
+export default Home
