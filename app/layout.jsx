4ebc210ccfa3ed601f813f8628ca8dcc7aa337fd
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-05-08 10:37:42 +0630

    Getting Client ID and Client Secret

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,15 +11,17 @@ const RootLayout = ( {children} ) => {
   return (
     <html lang='en'>
         <body>
-            <div className="main">
-                <div className="gradient"></div>
-            </div>
+            <Provider>
+                <div className="main">
+                    <div className="gradient"></div>
+                </div>
 
-            <main className="app">
-                < Nav/>
+                <main className="app">
+                    < Nav/>
 
-                {children}
-            </main>
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
