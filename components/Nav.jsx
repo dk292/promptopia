64a0129956b14394c8345216f245165c9b28ba0a
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-05-08 11:37:10 +0630

    Change user image base on google account

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -43,7 +43,7 @@ const Nav = () => {
                     </button>
 
                     <Link href="/profile">
-                        <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile'/>
+                        <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile'/>
                     </Link>
                 </div>
             ): (
@@ -61,7 +61,7 @@ const Nav = () => {
         <div className="sm:hidden flex relative">
             {session?.user ? (
                 <div className="flex">
-                    <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
+                    <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile' onClick={() => setToggleDropdown((prev) => !prev)}/>
 
                     {toggleDropdown && (
                         <div className='dropdown'>
